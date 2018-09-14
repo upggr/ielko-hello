afd639f78fc2d1ff3c0bdff26d097bb03bee1240
Ioannis Kokkinis <info@upg.gr>
2018-09-14 22:41:59 +0300

    Updated test.php.

--- test.php
+++ test.php
@@ -4,7 +4,7 @@
 * Plugin Name: Ielko test plugin
 * Plugin URI: https://www.ielko.com
 * Description: This is a test plugin by ielko
-* Version: 1.3.0
+* Version: 1.4.0
 * Author: Ioannis Kokkinis
 * Author URI: https://ielko.com
 * License: GPL2
@@ -22,5 +22,5 @@ add_action('wp_footer', 'my_function');
 
 function my_function()
 {
-    echo 'IELKO HELLO PLUGIN';
+    echo 'IELKO HELLO PLUGIN cfffff';
 }
