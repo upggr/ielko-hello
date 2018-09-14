cf82d5a78740cb52667a849670234fd610e5bf26
Ioannis Kokkinis <info@upg.gr>
2018-09-14 22:30:44 +0300

    Updated ielko-updater.phptest.php .

--- test.php
+++ test.php
@@ -4,11 +4,20 @@
 * Plugin Name: Ielko test plugin
 * Plugin URI: https://www.ielko.com
 * Description: This is a test plugin by ielko
-* Version: 1.0.0
+* Version: 1.1.0
 * Author: Ioannis Kokkinis
 * Author URI: https://ielko.com
 * License: GPL2
 */
+
+
+
+
+require_once('ielko-updater.php');
+if (is_admin()) {
+    new IelkoUpdater(__FILE__, 'myGitHubUsername', "Repo-Name");
+}
+
 add_action('wp_footer', 'my_function');
 
 function my_function()
