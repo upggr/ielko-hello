b2b626b3920d44e09220bbdee74b5b641d87b197
Ioannis Kokkinis <info@upg.gr>
2018-09-15 00:08:47 +0300

    Updated test.php.

--- test.php
+++ test.php
@@ -15,7 +15,7 @@
 
 require_once('ielko-updater.php');
 if (is_admin()) {
-    new IelkoUpdater(__FILE__, 'upggr', "ielko-hello");
+    new IelkoUpdater(__FILE__, 'upggr', "ielko-hello",);
 }
 
 add_action('wp_footer', 'my_function');
